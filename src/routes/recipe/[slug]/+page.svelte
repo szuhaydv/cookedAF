c75c3ba95434add364b91e51b29f4a3508223430
szuhaydv <szuhaydv@gmail.com>
2025-02-12 02:55:51 +0100

    add flier component mvp

---
+++ src/routes/recipe/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<main class="h-[calc(100dvh-4rem)]">
+  <ul class="bg-white w-[32rem] h-[40rem] relative pt-16 flex flex-col">
+    <img
+      src="/images/duct_tape.png"
+      alt="duct tape"
+      class="absolute top-[-4rem] left-1/2 -translate-x-1/2"
+    />
+    <h2 class="font-cheeseBread text-[3rem] text-center">Ingredients</h2>
+    <div
+      class="flex-1 mx-[10%] w-[80%]
+      bg-[repeating-linear-gradient(to_bottom,transparent_0px,transparent_47px,black_48px,black_48px)]"
+    ></div>
+  </ul>
+</main>
